9e9a8868ba082d2c650714799fa6e154afd84c80
zablon <zablonoigo51@gmail.com>
2026-03-15 13:04:25 +0300

    sales enrichment processor mask cutomer credit

---
+++ src/main/java/sales/dev/process/SalesEnrichmentProcessor.java
@@ -0,0 +1,41 @@
+package sales.dev.process;
+
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import sales.dev.avro.CustomerEvent;
+import sales.dev.avro.SalesEvent;
+
+public class SalesEnrichmentProcessor {
+
+    public static KStream<String, SalesEvent> enrichSales(
+            KStream<String, SalesEvent> sales,
+            KTable<String, CustomerEvent> customers,
+            Map<String, String> serdeConfig
+    ) {
+
+        SpecificAvroSerde<SalesEvent> salesSerde = new SpecificAvroSerde<>();
+        salesSerde.configure(serdeConfig, false);
+
+        SpecificAvroSerde<CustomerEvent> customerSerde = new SpecificAvroSerde<>();
+        customerSerde.configure(serdeConfig, false);
+
+        KStream<String, SalesEvent> maskedSales = sales.mapValues(sale -> {
+
+            String card = sale.getCardNumber();
+
+            if (card != null && card.length() > 4) {
+                String masked = "XXXX-XXXX-XXXX-" +
+                        card.substring(card.length() - 4);
+                sale.setCardNumber(masked);
+            }
+
+            return sale;
+        });
+    }
+}
