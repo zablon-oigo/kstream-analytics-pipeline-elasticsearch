4660248a1e7a7524f211fd244c0aff3ed8d2006e
zablon <zablonoigo51@gmail.com>
2026-03-22 21:28:56 +0300

    fix location processor

--- src/main/java/sales/dev/process/SalesLocationProcessor.java
+++ src/main/java/sales/dev/process/SalesLocationProcessor.java
@@ -1,17 +1,19 @@
 package sales.dev.process;
 
 import org.apache.kafka.streams.kstream.KStream;
-
+import sales.dev.avro.LocationEvent;
 import sales.dev.avro.SalesEvent;
 
 public class SalesLocationProcessor {
 
-    public static KStream<String, SalesEvent> extractLocation(
+    public static KStream<String, LocationEvent> extractLocation(
             KStream<String, SalesEvent> enrichedSales
     ) {
         return enrichedSales.mapValues(sale -> {
-            String geo = sale.getLatitude() + "," + sale.getLongitude();
-            return sale;
+            LocationEvent location = new LocationEvent();
+            location.setLatitude(sale.getLatitude());
+            location.setLongitude(sale.getLongitude());
+            return location;
         });
     }
 }
