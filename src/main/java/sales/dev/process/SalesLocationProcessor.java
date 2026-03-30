89ba90c04be7ad8aea8a8ae9ee88ddd17b2ed52b
zablon <zablonoigo51@gmail.com>
2026-03-30 23:28:41 +0300

    handle timestamp

--- src/main/java/sales/dev/process/SalesLocationProcessor.java
+++ src/main/java/sales/dev/process/SalesLocationProcessor.java
@@ -1,5 +1,7 @@
 package sales.dev.process;
 
+import java.time.Instant;
+
 import org.apache.kafka.streams.kstream.KStream;
 import sales.dev.avro.LocationEvent;
 import sales.dev.avro.SalesEvent;
@@ -13,6 +15,10 @@ public class SalesLocationProcessor {
             LocationEvent location = new LocationEvent();
             location.setLatitude(sale.getLatitude());
             location.setLongitude(sale.getLongitude());
+            if (sale.getTimestamp() != null) {
+                                Instant ts = Instant.parse(enrichedSales.getTimestamp().toString());
+                                location.setTimestamp(ts.toEpochMilli());
+                            }
             return location;
         });
     }
