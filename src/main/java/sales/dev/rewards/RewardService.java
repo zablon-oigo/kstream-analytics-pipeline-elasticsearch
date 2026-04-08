b4f5d59477e04bfbeeeabcbb471b59ee87152c2b
zablon <zablonoigo51@gmail.com>
2026-04-08 15:19:57 +0300

    delete files

--- src/main/java/sales/dev/rewards/RewardService.java
+++
@@ -1,9 +0,0 @@
-package sales.dev.rewards;
-
-public class RewardService {
-
-    public static void sendRewardEmail(String email, double totalSpend, double voucherAmount) {
-        System.out.printf("Reward email sent to %s! You spent KES %.2f. Voucher: KES %.2f for your next purchase.%n",
-                          email, totalSpend, voucherAmount);
-    }
-}
